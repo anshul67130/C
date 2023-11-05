1b6aeae1a3c6ed7592bc3e354418a180ce994aab
anshul <akofficial67130@gmail.com>
2023-11-05 22:40:19 +0530

    program edited

--- factorial.c
+++ factorial.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 int fact(int n){
-    if (n==1)
+    if (n==1 || n==0)
     {
         return 1;
-    }
-    else if (n==0)
-    {
-        // 
-        // printf("You can't enter zero\n");
-        return 0;
-        
     }
     return n*fact(n-1);
 }
