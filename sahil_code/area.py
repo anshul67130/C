f3ea2cd413c447309a39ade25b83023b7f8bb7d1
Anshul <116941502+anshul67130@users.noreply.github.com>
2024-01-03 01:52:05 +0530

    Delete sahil_code directory

--- sahil_code/area.py
+++
@@ -1,2 +0,0 @@
-n = int(input("Enter the radius"))
-print("area is",3.14*n*n)
--- sahil_code/area_of_circle.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main()
-{
-    float r,area;
-    printf("enter the radius of circle");
-    scanf("%f",&r);
-    area=3.14*r*r;
-    printf("\n the radius of circle is=%.2f",area);
-}
